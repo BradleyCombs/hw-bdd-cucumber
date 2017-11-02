1b377fde0f0f138704b119338a1dc612b8a18f10
Bradley Combs <bbcombs@uncc.edu>
2017-11-02 15:45:02 +0000

    Part 2: Complete

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -18,21 +18,38 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  assert page.body =~ /#{e1}.*#{e2}/m, "#{e1} did not come before #{e2}"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
-
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(',').each { |rating|
+    if uncheck
+      uncheck "ratings_#{rating}"
+    else
+      check "ratings_#{rating}"
+    end
+  }
 end
 
-Then /I should see all the movies/ do
+And /I hit submit button/ do
+  click_button("ratings_submit")
+end
+
+And /I should see all of the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  @movies = Movie.all
+  page.all('table#movies tr').count.should == @movies.size + 1
+  # assert page.body = "Not all movies listed."
+  #@movies.each { |movie|
+  #  if find_field(@title_header).value.not_include? movie
+  #    assert page.body = "#{movie.title} was not listed."
+  #  end
+  #  }
+  #end
 end
